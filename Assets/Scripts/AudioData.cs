d67f2b1d80128f9dafffaf0a0e70daa1b007583c
Alessandro <alessandro.fama@ymail.com>
2019-10-05 01:59:27 +0200

    Preparing AudioData ScriptableObject

---
+++ Assets/Scripts/AudioData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/AudioData", fileName = "New AudioData")]
+public class AudioData : ScriptableObject
+{
+    [Header("Player")]
+    [FMODUnity.EventRef]
+    public string test = null;
+
+    [Header("Environment")]
+    [FMODUnity.EventRef]
+    public string testEvent = null;
+
+    [Header("Music")]
+    [FMODUnity.EventRef]
+    public string music = null;
+
+    [Header("Local Parameters")]
+    public string localParameter = null;
+
+    [Header("Global Parameters")]
+    [FMODUnity.ParamRef]
+    public string parameter = null;
+
+}
