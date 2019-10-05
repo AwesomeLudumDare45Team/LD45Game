80b1bb7fe9cb26081b797d62639969174744f2aa
Eva <eva.gerbert.gaillard@gmail.com>
2019-10-05 05:25:07 +0200

    World Manager first attempt

---
+++ Assets/Scripts/WorldManager/ItemPickUp.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickUp : MonoBehaviour
+{
+    public List<WorldEffect> m_worldEffectList;
+    public bool m_debugDisableDestroy;
+    
+    void Start()
+    {
+        m_debugDisableDestroy = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PickUp();
+        }
+    }
+
+    public void PickUp()
+    {
+        foreach (WorldEffect worldEffect in m_worldEffectList)
+        {
+            if(worldEffect != null)
+            {
+                worldEffect.Execute();
+            }
+        }
+
+        if (!m_debugDisableDestroy)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/WorldManager/WorldManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldManager : MonoBehaviour
+{
+    private static WorldManager m_instance;
+
+    public GameObject m_wallUp;
+    public GameObject m_wallDown;
+    public GameObject m_wallRight;
+    public GameObject m_wallLeft;
+
+
+    private void Awake()
+    {
+        if (m_instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        m_instance = this;
+    }
+
+    public static WorldManager GetWorldManager(){
+        return m_instance;
+    }
+}
