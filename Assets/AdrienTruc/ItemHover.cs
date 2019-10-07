307b233078f2eec7c198893b9c13b53f6da97fce
aduporge <adrienvduporge@gmail.com>
2019-10-07 03:20:37 +0200

    AmazingItems

---
+++ Assets/AdrienTruc/ItemHover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHover : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		transform.position += new Vector3 (0, Mathf.Cos(Time.time) * 0.005f, 0);
+		transform.localEulerAngles += new Vector3(0, 1, 0);
+    }
+}
