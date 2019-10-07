5a396b2e58cead40537c4b8a887741ba5c26afb3
Nathan Desages <nathan.desages@gmail.com>
2019-10-07 18:12:13 +0200

    remove collider effect

---
+++ Assets/Scripts/WorldManager/WorldEffects/WorldEffectRemoveCollider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldEffectRemoveCollider : WorldEffect
+{
+	public GameObject obj;
+
+	public override void Initialize()
+	{
+	}
+
+	public override void Execute()
+	{
+		Destroy(obj.GetComponent<Collider>());
+	}
+}
